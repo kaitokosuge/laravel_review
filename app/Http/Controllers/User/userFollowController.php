d2f32955137f1dbc8fca7e5cd7e8f56a55a18e9a
kaitokosuge <kaitokosuge.mi@gmail.com>
2023-08-22 12:49:28 +0900

    フォロー機能モデル・マイグレーションファイル作成完了

--- app/Http/Controllers/User/userFollowController.php
+++ app/Http/Controllers/User/userFollowController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class userFollowController extends Controller
 {
-    public function index()
+    public function index(User $user)
     {
-        return view('User-follow.index');
+        return view('User-follow.index')->with(['users' => $user->get()]);
     }
 }
